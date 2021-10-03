733cc468f111141b3d8438afcbe605f613601bab
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-10-04 08:43:45 +0900

    Create main components and set up react router

---
+++ src/components/Status.tsx
@@ -0,0 +1,5 @@
+const Status = () => {
+  return <div>Status Component</div>
+};
+
+export default Status;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Router, BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Status from './components/Status';
+import Top from './components/Top';
+import Training from './components/Training';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/status">
+          <Status />
+        </Route>
+        <Route path="/training">
+          <Training />
+        </Route>
+        <Route path="/top">
+          <Top />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +31,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
