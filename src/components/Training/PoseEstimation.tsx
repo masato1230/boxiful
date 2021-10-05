79575b20193574623ab75f109361ecf00b80ebea
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-10-06 08:28:31 +0900

    Add mediapipe dependencies and show pose

--- src/components/Training/PoseEstimation.tsx
+++ src/components/Training/PoseEstimation.tsx
@@ -1,9 +1,84 @@
+import { useEffect, useRef } from 'react';
+import { Pose, Results, POSE_CONNECTIONS } from '@mediapipe/pose';
+import { Camera } from '@mediapipe/camera_utils';
+import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
+
 const PoseEstimation = () => {
+  const videoRef = useRef<any>();
+  const canvasRef = useRef<any>();
+
+  const onResults = (results: Results) => {
+    if (!results.poseLandmarks) {
+      return;
+    }
+    
+    const canvasCtx = canvasRef.current.getContext('2d');
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+    // Only overwrite existing pixels.
+    canvasCtx.globalCompositeOperation = 'source-in';
+    canvasCtx.fillStyle = '#00FF00';
+    canvasCtx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+    // Only overwrite missing pixels.
+    canvasCtx.globalCompositeOperation = 'destination-atop';
+    canvasCtx.drawImage(
+      results.image,
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+
+    canvasCtx.globalCompositeOperation = 'source-over';
+    drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
+      color: '#00FF00',
+      lineWidth: 4,
+    });
+    drawLandmarks(canvasCtx, results.poseLandmarks, {
+      color: '#FF0000',
+      lineWidth: 2,
+    });
+    canvasCtx.restore();
+    return;
+  };
+
+  // setup
+  useEffect(() => {
+    const pose = new Pose({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+      },
+    });
+    pose.setOptions({
+      modelComplexity: 1,
+      smoothLandmarks: true,
+      enableSegmentation: false,
+      smoothSegmentation: true,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+    pose.onResults(onResults);
+
+    const camera = new Camera(videoRef.current, {
+      onFrame: async () => {
+        await pose.send({ image: videoRef.current });
+      },
+      width: 1280,
+      height: 720,
+    });
+    camera.start();
+  }, []);
+
   return (
-    <div>
-      Pose
+    <div className="container">
+      <video ref={videoRef} className="hidden"></video>
+      <canvas
+        ref={canvasRef}
+      ></canvas>
     </div>
-  )
-}
+  );
+};
 
 export default PoseEstimation;
