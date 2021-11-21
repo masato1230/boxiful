8bf4c67db0c50edafc9c932b07571fd5d4c5a2ae
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-11-21 14:13:36 +0900

    Add privacySet JSX

--- src/models/privacies.tsx
+++ src/models/privacies.tsx
@@ -28,16 +28,16 @@ const purposeOfCollectingPersonalInformation: PrivacySet = {
   content: (
     <React.Fragment>
       <p>当サイトが個人情報を収集・利用する目的は、以下の通りです。</p>
-      <ol>
-        <li>当サイトサービスの提供・運営のため</li>
-        <li>メンテナンス、重要なお知らせなど必要に応じた連絡のため</li>
-        <li>
+      <ol className="list-decimal list-outside ml-8">
+        <li className="my-2" >当サイトサービスの提供・運営のため</li>
+        <li className="my-2" >メンテナンス、重要なお知らせなど必要に応じた連絡のため</li>
+        <li className="my-2" >
           利用規約に違反したユーザーや、不正・不当な目的でサービスを利用しようとするユーザーの特定をしご利用をお断りするため
         </li>
-        <li>
+        <li className="my-2" >
           ユーザーにご自身の登録情報の閲覧や変更、削除、ご利用状況の閲覧を行なっていただくため
         </li>
-        <li>上記の利用目的に付随する目的</li>
+        <li className="my-2" >上記の利用目的に付随する目的</li>
       </ol>
     </React.Fragment>
   ),
@@ -55,45 +55,45 @@ const changeOfPurposeOfUse: PrivacySet = {
 const ProvisionOfPersonalInformationToAThirdParth: PrivacySet = {
   title: '個人情報の第三者提供',
   content: (
-    <ol>
-      <li>
+    <ol className="list-decimal list-outside ml-8">
+      <li className="my-2" >
         <p>
           開発元は、次に掲げる場合を除いて、あらかじめユーザーの同意を得ることなく、第三者に個人情報を提供することはありません。ただし、個人情報保護法その他の法律で認められる場合を除きます。
         </p>
-        <ol>
-          <li>
+        <ol className="list-decimal list-outside ml-8">
+          <li className="my-2" >
             人の生命、身体または財産のために必要がある場合であって、本人の本人の同意を得ることが困難である時
           </li>
-          <li>
+          <li className="my-2" >
             公衆衛生の向上または児童の健全な育成の推進のために特に必要がある場合であっって、本人の同意を得ることが困難である時
           </li>
-          <li>
+          <li className="my-2" >
             国の機関もしくは地方公共団体またはその委託を受けた者が法令の定める事務を遂行することに対して協力する必要がある場合であって，本人の同意を得ることにより当該事務の遂行に支障を及ぼすおそれがあるとき
           </li>
-          <li>
+          <li className="my-2" >
             予め次の事項を告知あるいは公表し，かつ開発元が個人情報保護委員会に届出をしたとき
           </li>
-          <ol>
-            <li>利用目的に第三者への提供を含むこと</li>
-            <li>第三者に提供されるデータの項目</li>
-            <li>第三者への提供の手段または方法</li>
-            <li>本人の求めに応じて個人情報の第三者への提供を停止すること</li>
-            <li>本人の求めを受け付ける方法</li>
+          <ol className="list-decimal list-outside ml-8">
+            <li className="my-2" >利用目的に第三者への提供を含むこと</li>
+            <li className="my-2" >第三者に提供されるデータの項目</li>
+            <li className="my-2" >第三者への提供の手段または方法</li>
+            <li className="my-2" >本人の求めに応じて個人情報の第三者への提供を停止すること</li>
+            <li className="my-2" >本人の求めを受け付ける方法</li>
           </ol>
         </ol>
       </li>
-      <li>
+      <li className="my-2" >
         <p>
           前項の定めにかかわらず，次に掲げる場合には，当該情報の提供先は第三者に該当しないものとします。
         </p>
-        <ol>
-          <li>
+        <ol className="list-decimal list-outside ml-8">
+          <li className="my-2" >
             当社が利用目的の達成に必要な範囲内において個人情報の取扱いの全部または一部を委託する場合
           </li>
-          <li>
+          <li className="my-2" >
             合併その他の事由による事業の承継に伴って個人情報が提供される場合
           </li>
-          <li>
+          <li className="my-2" >
             個人情報を特定の者との間で共同して利用する場合であって，その旨並びに共同して利用される個人情報の項目，共同して利用する者の範囲，利用する者の利用目的および当該個人情報の管理について責任を有する者の氏名または名称について，あらかじめ本人に通知し，または本人が容易に知り得る状態に置いた場合
           </li>
         </ol>
@@ -158,11 +158,11 @@ const disclaimer: PrivacySet = {
 const changeOfPrivacyPolicy: PrivacySet = {
   title: 'プライバシーポリシーの変更',
   content: (
-    <ol>
-      <li>
+    <ol className="list-decimal list-outside ml-8">
+      <li className="my-2" >
         本ポリシーの内容は，法令その他本ポリシーに別段の定めのある事項を除いて，ユーザーに通知することなく，変更することができるものとします。
       </li>
-      <li>
+      <li className="my-2" >
         当社が別途定める場合を除いて，変更後のプライバシーポリシーは，本ウェブサイトに掲載したときから効力を生じるものとします。
       </li>
     </ol>
