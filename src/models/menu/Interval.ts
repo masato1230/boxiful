5e7d6c04de04e7881816235a70e80363f6e01045
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-12-03 00:04:56 +0900

    Add intervals

--- src/models/menu/Interval.ts
+++ src/models/menu/Interval.ts
@@ -1,4 +1,21 @@
+import Menu from './menu';
+
 export default interface Interval {
   durationMinutes: number;
   durationSeconds: number;
+}
+
+export const thirtySecondsInterval: Interval = {
+  durationMinutes: 0,
+  durationSeconds: 30,
+}
+
+export const fortyFiveSecondsInterval: Interval = {
+  durationMinutes: 0,
+  durationSeconds: 45,
+}
+
+export const oneMinuteInterval: Interval = {
+  durationMinutes: 1,
+  durationSeconds: 0,
 }
